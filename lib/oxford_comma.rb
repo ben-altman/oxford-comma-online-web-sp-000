a7cbc2c19b35b048bddb0af0c6ad8081b91a7792
Ben Altman <altmanguitar@gmail.com>
2020-11-08 03:54:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,28 +1,3 @@
-=begin
-def oxford_comma(array)
-  return array.join(' and ') if array.size < 3
-  array[-1] = "and " + array[-1]
-  array.join(', ')
-end
-=end
-
-=begin
-def oxford_comma(array)
-  array = [*array]
-  case array.size
-  when 0
-    ''
-  when 1
-    array[0].to_s
-  when 2
-    array.join(' and ')
-  else
-    array_copy = array
-    array_copy[-1] = "and #{array_copy[-1]}"
-    array_copy.join(', ')
-  end
-end
-=end
 
 def oxford_comma(array)
   if array.length == 2
